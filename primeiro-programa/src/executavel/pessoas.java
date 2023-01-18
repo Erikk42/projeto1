1ccfcd7df5ea92a0e881bf4f2ba9c41dc39f0ed6
Erikk42 <93799974+Erikk42@users.noreply.github.com>
2023-01-18 16:19:52 -0300

    atualizacao de cpf para idade nas strings

--- primeiro-programa/src/executavel/pessoas.java
+++ primeiro-programa/src/executavel/pessoas.java
@@ -1,8 +1,43 @@
 package executavel;
 
 public class pessoas {
-	public static void main(String[] args) {
-		System.out.println("meu primeiro programa em java");
+	
+	private String nome;
+	
+	private String sobrenome;
+	
+	private String cpf;
+	
+
+	public String getCpf() {
+		return cpf;
 	}
 
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+		
+	
+	}
+	
+
+	
+	
+	
+	
 }
